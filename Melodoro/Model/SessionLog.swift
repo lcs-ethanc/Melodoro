68a67e3919c2e3bcf45f398800bd553730068290
Ethan Chen <echen@lcs.on.ca>
2025-05-21 11:30:50 -0400

    made sessionlog conform to codable

--- Melodoro/Model/SessionLog.swift
+++ Melodoro/Model/SessionLog.swift
@@ -7,10 +7,24 @@
 
 import Foundation
 
-struct SessionLog: Identifiable {
-    let id = UUID()
+struct SessionLog: Identifiable, Codable {
+    let id : UUID
     let sessionDate: Date
     let focusDuration: Int
     let breakDuration: Int
     let completed: Bool
+    
+    init(
+        id: UUID,
+        sessionDate: Date,
+        focusDuration: Int,
+        breakDuration: Int,
+        completed: Bool
+    ) {
+        self.id = id
+        self.sessionDate = sessionDate
+        self.focusDuration = focusDuration
+        self.breakDuration = breakDuration
+        self.completed = completed
+    }
 }
